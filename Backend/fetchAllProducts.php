c42709742a0965b358a485ca92980ec6e6b506c0
kebal khadka <kebal khadka>
2024-05-16 16:42:43 +0545

    updated

--- Backend/fetchAllProducts.php
+++ Backend/fetchAllProducts.php
@@ -7,13 +7,12 @@ header("Content-Type: application/json; charset=UTF-8");
 
 class Constants
 {
-    static $DB_SERVER="localhost";
-    static $DB_NAME="ecommerce";
-    static $USERNAME="root";
-    static $PASSWORD="";
+    static $DB_SERVER = "localhost";
+    static $DB_NAME = "ecommerce";
+    static $USERNAME = "root";
+    static $PASSWORD = "";
 
-    static $SQL_SELECT_ALL="SELECT * FROM products";
-
+    static $SQL_SELECT_ALL = "SELECT * FROM products";
 }
 
 class Register
@@ -21,41 +20,33 @@ class Register
 
     public function connect()
     {
-        $con=new mysqli(Constants::$DB_SERVER,Constants::$USERNAME,Constants::$PASSWORD,Constants::$DB_NAME);
-        if($con->connect_error)
-        {
+        $con = new mysqli(Constants::$DB_SERVER, Constants::$USERNAME, Constants::$PASSWORD, Constants::$DB_NAME);
+        if ($con->connect_error) {
             return null;
-        }else
-        {
+        } else {
             return $con;
         }
     }
     public function select()
     {
-        $con=$this->connect();
-        if($con != null)
-        {
-            $result=$con->query(Constants::$SQL_SELECT_ALL);
-            if($result->num_rows>0)
-            {
-                $reg=array();
-                while($row=$result->fetch_array())
-                {
-                    array_push($reg, array("id"=>$row['id'],"name"=>$row['name'],"details"=>$row['details'],"description"=>$row['description'],"price"=>$row['price'],"image"=>$row['image'],"image1"=>$row['image1']));
+        $con = $this->connect();
+        if ($con != null) {
+            $result = $con->query(Constants::$SQL_SELECT_ALL);
+            if ($result->num_rows > 0) {
+                $reg = array();
+                while ($row = $result->fetch_array()) {
+                    array_push($reg, array("id" => $row['id'], "name" => $row['name'], "details" => $row['details'], "description" => $row['description'], "price" => $row['price'], "image" => $row['image'], "image1" => $row['image1']));
                 }
                 print(json_encode(array_reverse($reg)));
-            }else
-            {
+            } else {
                 print(json_encode(array("No records found")));
             }
             $con->close();
-
-        }else{
+        } else {
             print(json_encode(array("PHP EXCEPTION : CAN'T CONNECT TO MYSQL. NULL CONNECTION.")));
         }
     }
 }
-$reg=new Register();
+$reg = new Register();
 $reg->select();
-
 ?>
